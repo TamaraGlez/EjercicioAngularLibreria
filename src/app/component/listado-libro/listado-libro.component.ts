a59be8dcdf6ef2f3c588f26df1d08eb55b278614
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2023-02-05 17:19:51 +0100

    Style&Research

--- src/app/component/listado-libro/listado-libro.component.ts
+++ src/app/component/listado-libro/listado-libro.component.ts
@@ -1,28 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-listado-libro',
   templateUrl: './listado-libro.component.html',
   styleUrls: ['./listado-libro.component.scss']
 })
-export class ListadoLibroComponent implements OnInit {
+export class ListadoLibroComponent {
   //librosss es el mismo nombre de la clave del html de libreria y es la info q ue nos trae el padre por el input(DEL PADRE)
 
-  // recogemos con el input los librosss del padre y las dos variables que van cambiando todos los libros y los filtrados
+  //  nunca cambia y vien del padre recogemos con el input los librosss del padre y las dos variables que van cambiando todos los libros y los filtrados
   @Input() public librosss!: any
-  // al principio tiene los libros de arriba pero cuando usamos el evento filtra y muestra los filtrados
+  //cambiante y viene del padre al principio tiene los libros que le vienen del padre, y si añadimos uno nuevo también le suma, pero cuando filtramos se quedan los filtrados
   @Input() public librosssFiltered!: any
 
-    // copiamos el array libros en librosssFiltered
-
-    ngOnInit() {
-    this.librosssFiltered = [...this.librosss]
-    }
-
-  // creamos este método para filtrar los libros por este va
+  // creamos este método para filtrar los libros
 
   buscar (value: string) {
-  this.librosssFiltered = this.librosss.filter((libro: any) => libro.name.toLowerCase().includes(value.toLowerCase()))
+    this.librosssFiltered =
+    this.librosss.filter((libro: any) => libro.name.toLowerCase().includes(value.toLowerCase()))
   }
 
 }
---
+++ src/app/interfaz/ilibro.ts
@@ -0,0 +1,7 @@
+export interface ILibro {
+  name: string,
+  autor: string,
+  year: "number",
+  image: string,
+
+}
